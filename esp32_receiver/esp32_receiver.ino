8bab82e09680f7cc2fb04d360622e9e5e14c7127
Diogo Marciano <diogoam@sapo.pt>
2025-12-01 15:06:46 +0000

    Update esp32_receiver.ino

--- esp32_receiver/esp32_receiver.ino
+++ esp32_receiver/esp32_receiver.ino
@@ -1,1 +1,37 @@
+#include <HardwareSerial.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
+
+HardwareSerial LoraSerial(1); // UART1
+const char* ssid = "---";
+const char* password = "---";
+String googleScriptURL = "---";
+
+void setup() {
+  Serial.begin(115200);
+  LoraSerial.begin(9600, SERIAL_8N1, 16, 17); // RX=16, TX=17
+  WiFi.begin(ssid, password);
+  Serial.println("A ligar ao WiFi...");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("WiFi conectado.");
+}
+
+void loop() {
+  if (LoraSerial.available()) {
+    String msg = LoraSerial.readStringUntil('\n');
+    Serial.println("Recebido: " + msg);
+
+    if(WiFi.status() == WL_CONNECTED){
+      HTTPClient http;
+      String url = googleScriptURL + "?data=" + msg;
+      http.begin(url);
+      int httpResponseCode = http.GET();
+      Serial.println("HTTP response: " + String(httpResponseCode));
+      http.end();
+    }
+  }
+}
 
